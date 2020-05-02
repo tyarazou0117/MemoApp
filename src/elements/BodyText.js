1766e5fc13a6d96d0465e0b21f23a42d805f23b4
tyarazou0117 <tyara_zou_0117@yahoo.co.jp>
2020-05-02 13:06:44 +0900

    Create MemoList

---
+++ src/elements/BodyText.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet,View,Text } from 'react-native';
+
+class BodyText extends React.Component{
+  render(){
+    return(
+      <View>
+      <Text style={styles.Text}>
+      {this.props.children}
+      </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+text:{
+  color: '#DDD',
+  backgroundColor:'#eee',
+  alignItems: 'center',
+  justifyContent: 'center',
+},
+
+});
+
+export default BodyText;
